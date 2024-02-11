fc3e35e6da032ce47adcf51c790326201117376c
stagas <gstagas@gmail.com>
2024-02-11 19:33:26 +0200

    save

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 import '@total-typescript/ts-reset'
 import 'vitest/globals'
+import 'vitest/importMeta'
 declare global {
   interface ErrorConstructor {
     captureStackTrace(thisArg: any, func: any): void
